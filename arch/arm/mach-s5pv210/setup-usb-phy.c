6a568f165f50329f23979eefd152c629218de009
Mike Dyer <mike.dyer@md-soft.co.uk>
2012-08-12 15:58:17 +0100

    Add EHCI support

--- arch/arm/mach-s5pv210/setup-usb-phy.c
+++ arch/arm/mach-s5pv210/setup-usb-phy.c
@@ -14,58 +14,75 @@
 #include <linux/platform_device.h>
 #include <mach/map.h>
 #include <mach/regs-sys.h>
+#include <mach/regs-usb-phy.h>
 #include <plat/cpu.h>
 #include <plat/regs-usb-hsotg-phy.h>
 #include <plat/usb-phy.h>
 
-static int s5pv210_usb_otgphy_init(struct platform_device *pdev)
+static atomic_t host_usage;
+
+static int s5pv210_usb_host_phy_is_on(void)
 {
-	struct clk *xusbxti;
+	return (readl(S5PV210_PHYPWR) & PHY1_STD_ANALOG_POWERDOWN) ? 0 : 1;
+}
+
+static void s5pv210_usb_phy_clkset(struct platform_device *pdev)
+{
+	struct clk *xusbxti_clk;
 	u32 phyclk;
 
-	writel(readl(S5PV210_USB_PHY_CON) | S5PV210_USB_PHY0_EN,
-			S5PV210_USB_PHY_CON);
-
 	/* set clock frequency for PLL */
-	phyclk = readl(S3C_PHYCLK) & ~S3C_PHYCLK_CLKSEL_MASK;
+	phyclk = readl(S3C_PHYCLK) & ~CLKSEL_MASK;
 
-	xusbxti = clk_get(&pdev->dev, "xusbxti");
-	if (xusbxti && !IS_ERR(xusbxti)) {
-		switch (clk_get_rate(xusbxti)) {
+	xusbxti_clk = clk_get(&pdev->dev, "xusbxti");
+	if (xusbxti_clk && !IS_ERR(xusbxti_clk)) {
+		switch (clk_get_rate(xusbxti_clk)) {
 		case 12 * MHZ:
-			phyclk |= S3C_PHYCLK_CLKSEL_12M;
+			phyclk |= CLKSEL_12M;
 			break;
 		case 24 * MHZ:
-			phyclk |= S3C_PHYCLK_CLKSEL_24M;
+			phyclk |= CLKSEL_24M;
 			break;
 		default:
 		case 48 * MHZ:
 			/* default reference clock */
 			break;
 		}
-		clk_put(xusbxti);
+		clk_put(xusbxti_clk);
 	}
 
-	/* TODO: select external clock/oscillator */
-	writel(phyclk | S3C_PHYCLK_CLK_FORCE, S3C_PHYCLK);
+	writel(phyclk, S3C_PHYCLK);
+}
 
-	/* set to normal OTG PHY */
-	writel((readl(S3C_PHYPWR) & ~S3C_PHYPWR_NORMAL_MASK), S3C_PHYPWR);
-	mdelay(1);
+static int s5pv210_usb_phy0_init(struct platform_device *pdev)
+{
+	u32 rstcon;
 
-	/* reset OTG PHY and Link */
-	writel(S3C_RSTCON_PHY | S3C_RSTCON_HCLK | S3C_RSTCON_PHYCLK,
-			S3C_RSTCON);
-	udelay(20);	/* at-least 10uS */
-	writel(0, S3C_RSTCON);
+	dev_info(&pdev->dev, "phy[0] init\n");
+
+	writel(readl(S5PV210_USB_PHY_CON) | S5PV210_USB_PHY0_EN,
+			S5PV210_USB_PHY_CON);
+
+	s5pv210_usb_phy_clkset(pdev);
+
+	/* set to normal PHY0 */
+	writel((readl(S5PV210_PHYPWR) & ~PHY0_NORMAL_MASK), S5PV210_PHYPWR);
+
+	/* reset PHY0 and Link */
+	rstcon = readl(S5PV210_PHYPWR) | PHY0_SWRST_MASK;
+	writel(rstcon, S5PV210_PHYPWR);
+	udelay(10);
+
+	rstcon &= ~PHY0_SWRST_MASK;
+	writel(rstcon, S5PV210_PHYPWR);
 
 	return 0;
 }
 
-static int s5pv210_usb_otgphy_exit(struct platform_device *pdev)
+static int s5pv210_usb_phy0_exit(struct platform_device *pdev)
 {
-	writel((readl(S3C_PHYPWR) | S3C_PHYPWR_ANALOG_POWERDOWN |
-				S3C_PHYPWR_OTG_DISABLE), S3C_PHYPWR);
+	writel((readl(S5PV210_PHYPWR) | PHY0_ANALOG_POWERDOWN |
+				PHY0_OTG_DISABLE), S5PV210_PHYPWR);
 
 	writel(readl(S5PV210_USB_PHY_CON) & ~S5PV210_USB_PHY0_EN,
 			S5PV210_USB_PHY_CON);
@@ -73,18 +90,101 @@ static int s5pv210_usb_otgphy_exit(struct platform_device *pdev)
 	return 0;
 }
 
+static int s5pv210_usb_phy1_init(struct platform_device *pdev)
+{
+	struct clk *otg_clk;
+	u32 rstcon;
+	int err;
+
+	dev_info(&pdev->dev, "phy[1] init\n");
+
+	atomic_inc(&host_usage);
+
+	otg_clk = clk_get(&pdev->dev, "otg");
+	if (IS_ERR(otg_clk)) {
+		dev_err(&pdev->dev, "Failed to get otg clock\n");
+		return PTR_ERR(otg_clk);
+	}
+
+	err = clk_enable(otg_clk);
+	if (err) {
+		clk_put(otg_clk);
+		return err;
+	}
+
+	if (s5pv210_usb_host_phy_is_on())
+		return 0;
+
+	writel(readl(S5PV210_USB_PHY_CON) | S5PV210_USB_PHY1_EN,
+			S5PV210_USB_PHY_CON);
+
+	s5pv210_usb_phy_clkset(pdev);
+
+	/* set to normal standard USB of PHY1 */
+	writel((readl(S5PV210_PHYPWR) & ~PHY1_STD_NORMAL_MASK), S5PV210_PHYPWR);
+
+	/* reset all ports of both PHY and Link */
+	rstcon = readl(S5PV210_RSTCON) | PHY1_SWRST_MASK;
+	writel(rstcon, S5PV210_RSTCON);
+	udelay(10);
+
+	rstcon &= ~PHY1_SWRST_MASK;
+	writel(rstcon, S5PV210_RSTCON);
+	udelay(80);
+
+	clk_disable(otg_clk);
+	clk_put(otg_clk);
+
+	return 0;
+}
+
+static int s5pv210_usb_phy1_exit(struct platform_device *pdev)
+{
+	struct clk *otg_clk;
+	int err;
+
+	if (atomic_dec_return(&host_usage) > 0)
+		return 0;
+
+	otg_clk = clk_get(&pdev->dev, "otg");
+	if (IS_ERR(otg_clk)) {
+		dev_err(&pdev->dev, "Failed to get otg clock\n");
+		return PTR_ERR(otg_clk);
+	}
+
+	err = clk_enable(otg_clk);
+	if (err) {
+		clk_put(otg_clk);
+		return err;
+	}
+
+	writel((readl(S5PV210_PHYPWR) | PHY1_STD_ANALOG_POWERDOWN),
+			S5PV210_PHYPWR);
+
+	writel(readl(S5PV210_USB_PHY_CON) & ~S5PV210_USB_PHY1_EN,
+			S5PV210_USB_PHY_CON);
+
+	clk_disable(otg_clk);
+	clk_put(otg_clk);
+
+	return 0;
+}
+
 int s5p_usb_phy_init(struct platform_device *pdev, int type)
 {
 	if (type == S5P_USB_PHY_DEVICE)
-		return s5pv210_usb_otgphy_init(pdev);
-
+		return s5pv210_usb_phy0_init(pdev);
+	else if (type == S5P_USB_PHY_HOST)
+		return s5pv210_usb_phy1_init(pdev);
 	return -EINVAL;
 }
 
 int s5p_usb_phy_exit(struct platform_device *pdev, int type)
 {
 	if (type == S5P_USB_PHY_DEVICE)
-		return s5pv210_usb_otgphy_exit(pdev);
+		return s5pv210_usb_phy0_exit(pdev);
+	else if (type == S5P_USB_PHY_HOST)
+		return s5pv210_usb_phy1_exit(pdev);
 
 	return -EINVAL;
 }
